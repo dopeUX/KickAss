4a74cce0ef8eb4f4972d32304b10e5c86b8e20ed
UXNation <uxnation.inc@gmail.com>
2022-03-11 17:03:11 +0530

    final

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -4,7 +4,7 @@ const address =  process.env.ADDRESS;
 
 const instance = new web3.eth.Contract(
      campaignFactory.abi,
-     address
+     '0xb4A6F688B89a6f3161cC615Dbee6b05a73Aa8948'
 );
 // console.log(instance.methods.getDeployedCampaigns())
 
