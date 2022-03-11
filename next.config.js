c58c1e69faf3cc474d981a6ccca09cac4a215543
UXNation <uxnation.inc@gmail.com>
2022-03-11 17:20:39 +0530

    init

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // target: 'serverless',
-  // env: {
-  //   ADDRESS: process.env.CONTRACT_ADDRESS,
-  // },
+  target: 'serverless',
+  env: {
+    ADDRESS: process.env.CONTRACT_ADDRESS,
+  },
 }
 
 module.exports = nextConfig
